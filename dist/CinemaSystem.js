b0f2b839c4236529996de0c8e8459a0cc9fab27f
Tomoya <flyingfox193807@gmail.com>
2025-02-07 12:27:53 -0800

    finished Cinema

--- dist/CinemaSystem.js
+++ dist/CinemaSystem.js
@@ -9,16 +9,68 @@
 var MovieGenre;
 (function (MovieGenre) {
     MovieGenre[MovieGenre["Action"] = 0] = "Action";
-    // add 4 more
+    MovieGenre[MovieGenre["Romance"] = 1] = "Romance";
+    MovieGenre[MovieGenre["SF"] = 2] = "SF";
+    MovieGenre[MovieGenre["Fantasy"] = 3] = "Fantasy";
+    MovieGenre[MovieGenre["Comedy"] = 4] = "Comedy";
 })(MovieGenre || (MovieGenre = {}));
 var movies = [];
 function addMovie(movieId, title, genre, availableSeats) {
+    var newMovie = { movieId: movieId, title: title, genre: genre, availableSeats: availableSeats };
+    movies.push(newMovie);
+    return newMovie;
 }
 function bookSeat(movieId, rowLetter, seatNumber) {
+    var thisMovie;
+    for (var i = 0; i < movies.length; i++) {
+        if (movies[i].movieId === movieId) {
+            thisMovie = movies[i];
+            break;
+        }
+    }
+    if (!thisMovie) {
+        return 'Movie not found.';
+    }
+    var thisSeat;
+    if (thisMovie && thisMovie.availableSeats.length > 0) {
+        for (var i = 0; i < thisMovie.availableSeats.length; i++) {
+            if (thisMovie.availableSeats[i][0] === rowLetter && thisMovie.availableSeats[i][1] === seatNumber) {
+                thisSeat = thisMovie.availableSeats[i];
+            }
+        }
+        if (!thisSeat) {
+            return "Seat ".concat(rowLetter).concat(seatNumber, " not available.");
+        }
+        thisMovie.availableSeats = thisMovie.availableSeats.filter(function (seat) { return seat[0] !== rowLetter || seat[1] !== seatNumber; });
+        return "Seat ".concat(rowLetter).concat(seatNumber, " booked successfully.");
+    }
+    else {
+        return 'No seats available.';
+    }
 }
 function checkSeatAvailability(movieId, rowLetter, seatNumber) {
+    var thisMovie;
+    for (var i = 0; i < movies.length; i++) {
+        if (movies[i].movieId === movieId) {
+            thisMovie = movies[i];
+            break;
+        }
+    }
+    if (!thisMovie) {
+        return 'Movie not found.';
+    }
+    if (thisMovie && thisMovie.availableSeats.length > 0) {
+        for (var i = 0; i < thisMovie.availableSeats.length; i++) {
+            if (thisMovie.availableSeats[i][0] === rowLetter && thisMovie.availableSeats[i][1] === seatNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+    return 'No seats available.';
 }
 // Test cases (Create more if needed)
 console.log(addMovie(1, "Avengers", MovieGenre.Action, [["A", 1], ["A", 2]])); // { movieId: 1, title: "Avengers", genre: MovieGenre.Action, availableSeats: [["A", 1], ["A", 2]] }
 console.log(bookSeat(1, "A", 1)); // "Seat A1 booked successfully"
 console.log(checkSeatAvailability(1, "A", 1)); // false
+console.log(movies[0].availableSeats);
--- src/CinemaSystem.ts
+++ src/CinemaSystem.ts
@@ -8,30 +8,88 @@
 
 enum MovieGenre {
   Action,
-  // add 4 more
+  Romance,
+  SF,
+  Fantasy,
+  Comedy
 }
 
 type Seat = [string, number]
 
 type Movie = {
-
+  movieId: number,
+  title: string,
+  genre: MovieGenre,
+  availableSeats: Seat[]
 }
 
 const movies: Movie[] = [];
 
-function addMovie(movieId, title, genre, availableSeats) {
-
+function addMovie(movieId: number, title: string, genre: MovieGenre, availableSeats: Seat[]): Movie {
+  let newMovie = { movieId, title, genre, availableSeats }
+  movies.push(newMovie)
+  return newMovie
 }
 
-function bookSeat(movieId, rowLetter, seatNumber) {
+function bookSeat(movieId: number, rowLetter: string, seatNumber: number): string {
+  let thisMovie: Movie | undefined
+  for (let i = 0; i < movies.length; i++) {
+    if (movies[i].movieId === movieId) {
+      thisMovie = movies[i]
+      break
+    }
+  }
 
+  if (!thisMovie) {
+    return 'Movie not found.'
+  }
+
+  let thisSeat: Seat | undefined
+  if (thisMovie && thisMovie.availableSeats.length > 0) {
+    for (let i = 0; i < thisMovie.availableSeats.length; i++) {
+      if (thisMovie.availableSeats[i][0] === rowLetter && thisMovie.availableSeats[i][1] === seatNumber ) {
+        thisSeat = thisMovie.availableSeats[i]
+      }
+    }
+
+    if (!thisSeat) {
+      return `Seat ${rowLetter}${seatNumber} not available.`
+    }
+    
+    thisMovie.availableSeats = thisMovie.availableSeats.filter(seat => seat[0] !== rowLetter || seat[1] !== seatNumber)
+    return `Seat ${rowLetter}${seatNumber} booked successfully.`
+  } else {
+    return 'No seats available.'
+  }
 }
 
-function checkSeatAvailability(movieId, rowLetter, seatNumber) {
+function checkSeatAvailability(movieId: number, rowLetter: string, seatNumber: number): boolean | string {
+  let thisMovie: Movie | undefined
+  for (let i = 0; i < movies.length; i++) {
+    if (movies[i].movieId === movieId) {
+      thisMovie = movies[i]
+      break
+    }
+  }
 
+  if (!thisMovie) {
+    return 'Movie not found.'
+  }
+
+  if (thisMovie && thisMovie.availableSeats.length > 0) {
+    for (let i = 0; i < thisMovie.availableSeats.length; i++) {
+      if (thisMovie.availableSeats[i][0] === rowLetter && thisMovie.availableSeats[i][1] === seatNumber ) {
+        return true
+      }
+    }
+    return false
+  }
+
+  return 'No seats available.'
 }
 
 // Test cases (Create more if needed)
 console.log(addMovie(1, "Avengers", MovieGenre.Action, [["A", 1], ["A", 2]])) // { movieId: 1, title: "Avengers", genre: MovieGenre.Action, availableSeats: [["A", 1], ["A", 2]] }
 console.log(bookSeat(1, "A", 1)) // "Seat A1 booked successfully"
 console.log(checkSeatAvailability(1, "A", 1)) // false
+console.log(movies[0].availableSeats)
